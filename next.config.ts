dadce164a275d396ddcbfcc1765cc3f0a21eaf7c
Ermias Bunaro <ermiarch@gmail.com>
2024-12-26 15:03:26 +0300

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/.' : '',
+  trailingSlash: true
 };
 
 export default nextConfig;
